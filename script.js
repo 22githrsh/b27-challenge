5a0809c3a06d58b7c38d25020816393262cf69e9
Harsh sen <senh2675@gmail.com>
2024-03-03 13:15:33 +0530

    filler

---
+++ script.js
@@ -0,0 +1,32 @@
+const postsData = [
+    {
+        username: "Sarthak",
+        dp: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 56
+    },
+    {
+        username: "Akarsh",
+        dp: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 125
+    },
+    {
+        username: "Sarthak",
+        dp: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 789
+    },
+    {
+        username: "vedant",
+        dp: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 23
+    },
+    {
+        username: "HARSHITA",
+        dp: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 58
+    },
+    // Add more posts as needed
+];
+
+
+// write your code here to create card
+
